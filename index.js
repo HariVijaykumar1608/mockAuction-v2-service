ca8581daa5189fe4078cbabdcd8bbaa62ccde805
Hariprasad <hariprasad.vijay2001@gmail.com>
2025-08-26 20:27:54 +0530

    createAndJoinRoomChanges (#2)

--- index.js
+++ index.js
@@ -1,49 +1,84 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const { customAlphabet } = require("nanoid");
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: { origin: "*" }
 });
 
-// Store count for each room
-const roomCounts = {};
+// Store members in each room
+const roomData = {};
+
+// Quick lookup: socketId → roomCode
+const userRoomMap = {};
 
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
-  socket.on("create-room", () => {
-    const roomCode = "1234"; // generate dynamically if needed
+  socket.on("create-room", (userData) => {
+    const nanoid = customAlphabet('0123456789', 4);
+    const roomCode = nanoid();
+
     socket.join(roomCode);
 
-    // initialize count
-    roomCounts[roomCode] = 0;
+    const userObj = { ...userData, _id: socket.id, isActive: true };
+    roomData[roomCode] = [userObj];
+    userRoomMap[socket.id] = roomCode;
 
     socket.emit("room-created", roomCode);
+    socket.emit("getAllRoomMembers", roomData[roomCode] || []);
   });
 
-  socket.on("join-room", (roomCode) => {
-    socket.join(roomCode);
-    socket.emit("joined-room",roomCode)
-    socket.emit("getCount", roomCounts[roomCode] || 0);
-  });
+  socket.on("join-room", (userData) => {
+    const options = [
+      "CSK", "MI", "RCB", "KKR", "GT", "LSG", "SRH", "PBKS", "RR", "DC"
+    ];
+    const { roomCode, team, userName } = userData;
+    const roomInfo = roomData[roomCode] || [];
 
-  socket.on("increment", (roomCode) => {
-    if (roomCounts[roomCode] !== undefined) {
-      roomCounts[roomCode] += 1;
-      io.to(roomCode).emit("getCount", roomCounts[roomCode]);
-    }
-  });
+    if (roomInfo) {
+      const teamExists = roomInfo.some((obj) => obj.team === team);
+      if (teamExists) {
+        let response = {
+          msg: "Selected team has Already taken!!",
+          option: options.filter((name) => name !== team),
+        };
+        socket.emit("joinRoomErrorHandling", response);
+      } else {
+        const userObj = {
+          userName,
+          team,
+          _id: socket.id,
+          isActive: true
+        };
+        roomData[roomCode]?.push(userObj);
+        userRoomMap[socket.id] = roomCode;
 
-  socket.on("decrement", (roomCode) => {
-    if (roomCounts[roomCode] !== undefined) {
-      roomCounts[roomCode] -= 1;
-      io.to(roomCode).emit("getCount", roomCounts[roomCode]);
+        socket.join(roomCode);
+        socket.emit("joined-room", roomCode);
+        io.to(roomCode).emit("getAllRoomMembers", roomData[roomCode] || []);
+      }
     }
   });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
+
+    const roomCode = userRoomMap[socket.id];
+    if (!roomCode) return;
+
+    const users = roomData[roomCode];
+    if (!users) return;
+
+    const userIndex = users.findIndex((u) => u._id === socket.id);
+    if (userIndex !== -1) {
+      roomData[roomCode][userIndex].isActive = false;
+      io.to(roomCode).emit("getAllRoomMembers", roomData[roomCode]);
+    }
+
+    // cleanup mapping
+    delete userRoomMap[socket.id];
   });
 });
 
